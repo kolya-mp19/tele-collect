db708616573ea510158eb80e5c1781867fcaca02
goloveshkin nikolay <ng@itglobal.ru>
2023-04-27 14:46:09 +0300

    logger

--- tele-collect-back/src/main.ts
+++ tele-collect-back/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
+    logger: ['log', 'debug', 'error', 'verbose', 'warn'],
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3001);
