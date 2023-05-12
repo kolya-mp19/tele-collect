d87adacc0b65a0ad5a35f6b1dcd67f518ad0bf22
goloveshkin nikolay <ng@itglobal.ru>
2023-05-12 15:25:30 +0300

    add snippets, remove husky, add first component

---
+++ tele-collect-front/components/auth/AuthPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ButtonAnt type="primary">Enjoy it</ButtonAnt>
+  </div>
+</template>
+
+<script>
+import { Button as ButtonAnt } from 'ant-design-vue'
+
+export default {
+  name: 'AuthPage',
+  components: {
+    ButtonAnt
+   }
+}
+</script>
+
+<!-- <style scoped>
+  .red {
+    color:red
+  }
+</style> -->
